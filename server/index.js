42abce192a1ad6bc13b2ac532fa6896451ced756
Dhruv Agja <dhruvmk46@gmail.com>
2024-02-29 22:01:42 +0530

    postgresql connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 
 const app = express();
 
@@ -7,7 +8,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 
+// create a student
+
+app.post('/students', async (req, res) => {
+    try{
+        // console.log(req.body.roll);
+        const name = req.body.name;
+        const roll = req.body.roll;
+        const dept = req.body.dept;
+
+        const newStudent = await pool.query("INSERT INTO student (roll, name, dept) VALUES($1, $2, $3) RETURNING *", [roll, name, dept]);
+        
+        res.json(newStudent);
+    }
+    catch (err){
+        console.error(err.message);
+    }
+})
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
