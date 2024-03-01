5d00418b3254fe47b81a614cf624ba8c500734ee
Mallikarjun-0 <saimallikarjun9@gmail.com>
2024-03-02 03:54:41 +0530

    added authorization to organized_events

--- server/routes/api/organized_events.js
+++ server/routes/api/organized_events.js
@@ -6,14 +6,20 @@ import express from 'express';
 import pool from '../../db.js';
 const router = express.Router();
 
+import authenticateToken from '../../auth.js';
+
 // display all organized events by organizer
-router.get('/:id', async (req, res) => {
+router.get('/:id', authenticateToken, async (req, res) => {
     try{
         const id = req.params.id;
+
+        if(id != req.user.id){
+            res.status(404).json({msg:"page not found"});
+        }
         const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
         const role = user.rows[0].role;
         if(role === 'organizer'){
-            const allOrganizedEvents = await pool.query("SELECT * FROM event JOIN student_manage on event.eid = student_manage.eid");
+            const allOrganizedEvents = await pool.query("SELECT * FROM event JOIN student_manage on event.eid = student_manage.eid where student_manage.id = $1", [id]);
             res.json(allOrganizedEvents.rows);
         }
         else{
