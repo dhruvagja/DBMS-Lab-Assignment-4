cb7139be3f61263e8a23f2f599ed1632d793ba28
ViNaYaK2703GuPtA <108051097+ViNaYaK2703GuPtA@users.noreply.github.com>
2024-03-01 22:48:43 +0530

    Third Commit to HomePage

--- client/my-app/src/components/Navbar.js
+++ client/my-app/src/components/Navbar.js
@@ -1,30 +1,29 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-title">
-        Home
-      </Link>
-      <ul>
-        <CustomLink to="/registered-events">Registered Events</CustomLink>
-        <CustomLink to="/organising-events">Organising Events</CustomLink>
-        <CustomLink to="/volunteer-events">Volunteering Events</CustomLink>
+      <div className="left-links">
+        <Link to="/" className="site-title">
+          Home
+        </Link>
+        <ul>
+          <CustomLink to="/registered-events">Registered Events</CustomLink>
+          <CustomLink to="/organising-events">Organising Events</CustomLink>
+          <CustomLink to="/volunteer-events">Volunteering Events</CustomLink>
+        </ul>
+      </div>
+      <div className="right-links">
         <CustomLink to="/logout">Logout</CustomLink>
-      </ul>
+      </div>
     </nav>
-  )
+  );
 }
 
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
+function CustomLink({ to, children }) {
   return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
+    <li>
+      <Link to={to}>{children}</Link>
     </li>
-  )
+  );
 }
