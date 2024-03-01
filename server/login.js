9d04c67ccc5a0ff062cec36f3b8c68a69b510856
Dhruv Agja <dhruvmk46@gmail.com>
2024-03-01 19:23:59 +0530

    deleted extra

--- server/login.js
+++ server/login.js
@@ -12,13 +12,13 @@ router.post('/',  async (req, res) => {
         const user = await pool.query("SELECT id,password from users where users.id = $1", [id]);
         
         if(user == null){
-            res.status(200).send("User not found");
+            res.status(400).send("User not found");
         }
 
         if(bcrypt.compare(req.body.password, user.rows[0].password)){
             res.status(200).send("User found");
         }else{
-            res.status(200).send("Wrong passoword");
+            res.status(500).send("Wrong passoword");
         }
 
 
