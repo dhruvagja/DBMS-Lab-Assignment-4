bc5356e96e467e9d21dc30cb2f88030a05a7e9ae
ViNaYaK2703GuPtA <108051097+ViNaYaK2703GuPtA@users.noreply.github.com>
2024-03-02 11:33:13 +0530

    Linking login page to home page

--- client/my-app/src/components/Home.js
+++ client/my-app/src/components/Home.js
@@ -1,35 +1,52 @@
 // Home.js
-
 import React from 'react';
 import './Home.css';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import RegisteredEvents from './RegisteredEvents';
 import OrganisingEvents from './OrganisingEvents';
 import VolunteerEvents from './VolunteerEvents';
 import Events from './Events';
 import Logout from './Logout';
 import Navbar from './Navbar';
+import { Navigate } from "react-router-dom";
+
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
-  return (
-    <div>
-      <Router>
-      <Navbar />
-      
-       <Routes>
-        <Route path="/" element={<Events />} />
-        <Route path="/registered-events" element={<RegisteredEvents />} />
-        <Route path="/organising-events" element={<OrganisingEvents />} />
-        <Route path="/volunteer-events" element={<VolunteerEvents />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes> 
-      
-      </Router>
-      
-    </div>
-  );
+
+  const [authenticated, setauthenticated] = useState(null);
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("authenticated");
+    if (loggedInUser) {
+      setauthenticated(loggedInUser);
+    }
+  }, []);
+
+  if (!authenticated) {
+    return <Navigate replace to="/login" />;
+  
+  }
+
+    // Access user credentials from local storage
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    const role = localStorage.getItem('role');
+
+    // You can use username, password, and role as needed within this component
+
+    return (
+        <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Events />} />
+                    <Route path="/registered-events" element={<RegisteredEvents />} />
+                    <Route path="/organising-events" element={<OrganisingEvents />} />
+                    <Route path="/volunteer-events" element={<VolunteerEvents />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Routes>
+        </div>
+    );
 };
 
 export default Home;
